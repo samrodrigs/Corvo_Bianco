ce45d96e93e389bc479c67a574b2ce5ad8905e5d
samrodrigs <samantharazevedo10@gmail.com>
2025-10-01 13:43:22 -0300

    continuando a campanha do geralt

--- geralt.py
+++ geralt.py
@@ -23,19 +23,39 @@ def geralt():
           '2 - Voltar para casa \n'
           '3 - Ir ao vinhedo')
 
+    escolha_feita1 = False
+    escolha_feita3 = False
+
     while True:
         escolha = input('Escolha:')
-        if escolha == '1':
-            print('Ao ficar você ver o céu se tornando escuro e a luz amarelada o sol tocar ')
-            break
-        elif escolha == '2':
+        if escolha == '1' and not escolha_feita1:
+            geralt_atributos["hp"] += 2
+            print('🌅 Você ganhou mais 2 Pontos de vida! Ao ficar você ver o céu se tornando escuro e a '
+            'luz amarelada do sol tocar o chão. A paz interior \n'
+            'toca seu peito e você respira ')
+            escolha_feita1 = True
+            print(geralt_atributos['hp'])
 
+        elif escolha == '2':
+            print('Quando o sol se vai, você desce aos poucos observando os musgos se apossarem das pedras, a lua ganhar força\n'
+                  ' e as luzes do Castelo de Beauclair se acenderem. Puxa o ar com força ao pisar nos pedregulhos '
+                  'que te guiam a casa principal.')
             break
-        elif escolha == '3'
+
+        elif escolha == '3' and not escolha_feita3:
+            geralt_atributos["itens"].append('Uvas de Corvo Bianco')
+            print('Você vai ao vinhedo, mas nada lá lhe parece diferente do que antes. Você vai até as vinhas, e pega algumas uvas\n'
+                  'colocando-as em seu saquinho de pano e logo em seguida de volta em seu gibão\n'
+                  'Você conseguiu:🍇 Uvas de Corvo Bianco ')
+            escolha_feita3 = True
+            print(geralt_atributos['itens'])
+
         else:
             print('Opção não existente')
 
-
+        if escolha_feita1 and escolha_feita3:
+            print('Já não resta nada para explorar agora\n'
+                  'Volte para casa. ')
 
 
 
